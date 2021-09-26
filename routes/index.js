fbb8af81a1d5476c4d1a4950b0ac887a2b856e8a
Eduardo Sa <edusrm11@gmail.com>
2021-09-26 17:34:14 -0300

    configuraçoes de rotas, jest, knex, estrutura

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const authRouter = require('./auth')
+const usersRouter = require('./users')
+
+router.use('/auth', authRouter)
+router.use('/users', usersRouter)
+
+router.get('/', (req, res, next)=>{
+  res.send('Rota index')
+})
+
+module.exports = router
