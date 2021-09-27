7989bc189b39ce8c2472dd0dcc2f802ad954cebd
Eduardo Sa <edusrm11@gmail.com>
2021-09-27 03:31:49 -0300

    refactor: routes, models, controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const authController = require('../controllers/auth.js')
 
 router.get('/',(req, res, next)=>{
   res.send('Rota auth')
 })
+
+router.get('/login',authController.login)
+
+router.get('/logout',authController.logout)
+
+router.get('/signup',authController.signup)
 
 module.exports = router
