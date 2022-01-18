4996550cf7b56c8eeee025c4cdf4ebf8d127e5e2
Eduardo Sa <edusrm11@gmail.com>
2022-01-18 19:30:12 -0300

    feature: commit inicial

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const routes = require('./routes')
 const app = express()
-app.use('',routes)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('',routes)
 
 
 // // catch 404 and forward to error handler
