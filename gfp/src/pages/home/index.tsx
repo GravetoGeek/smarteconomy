2adfc15e597a590b5222181fec2a24985e790472
Eduardo Sa <edusrm11@gmail.com>
2022-04-30 01:15:40 -0300

    feat: login system

--- gfp/src/pages/home/index.tsx
+++ gfp/src/pages/home/index.tsx
@@ -1,12 +1,31 @@
 import React from "react";
-import {View, Text, Image, TouchableOpacity, StyleSheet} from "react-native";
+import {Text, Image, TouchableOpacity, StyleSheet, ScrollView} from "react-native";
 import { styles } from "./style";
+import { useNavigation } from "@react-navigation/native";
+
+
+
 
 export default function Home() {
+  const navigation = useNavigation();
+
+function handleLogin() {
+  navigation.navigate("Login");
+}
+
   return (
-    <View style={styles.container}>
+    <ScrollView
+      contentContainerStyle={styles.container}
+    
+    >
       <Text style={styles.title}>Home</Text>
-    </View>
+      <TouchableOpacity
+        style={styles.btnLogin}
+        onPress={handleLogin}
+      >
+        <Text style={styles.txtWhite}>Home</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 }
 
--- gfp/src/pages/home/style.tsx
+++ gfp/src/pages/home/style.tsx
@@ -3,6 +3,7 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -11,4 +12,17 @@ export const styles = StyleSheet.create({
     color: '#636b6f',
     fontWeight: '100',
   },
+  btnLogin:{
+    width: '100%',
+    height: 40,
+    backgroundColor:'#bc14ff',
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  txtWhite:{
+    color:'white',
+    fontWeight:'bold',
+    fontSize:16,
+  }
 });
