44b9428613e375c8e3ee6c3e8184e0187321e3b0
Eduardo Sá <edusrm11@gmail.com>
2022-10-23 22:14:38 -0300

    feat: add gender, profile, category, account

--- backend/app/routes/index.ts
+++ backend/app/routes/index.ts
@@ -2,6 +2,11 @@ import { Router } from "express";
 import authRoute from "./authRoute";
 import {Request, Response} from 'express';
 import userRoute from "./userRoute";
+import accountRoute from "./accountRoute";
+import categoryRoute from "./categoryRoute";
+import profileRoute from "./profileRoute";
+import genderRoute from "./genderRoute";
+
 const router = Router();
 
 // router.use('/user',userRoute)
@@ -12,6 +17,10 @@ router.get('/', (req, res) => {
 
 router.use('/auth',authRoute)
 router.use('/user',userRoute)
+router.use('/account',accountRoute)
+router.use('/category',categoryRoute)
+router.use('/profile',profileRoute)
+router.use('/gender',genderRoute)
 
 router.use((req:Request, res:Response, next) => {
   res.status(404).send('404 Not Found');
