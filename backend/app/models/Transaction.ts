b2422212636d79a031ee68dbe1ed5e14d3b69c88
Eduardo Sá <edusrm11@gmail.com>
2022-11-13 01:12:51 -0300

    refactor: tipo de transacao adicionado

--- backend/app/models/Transaction.ts
+++ backend/app/models/Transaction.ts
@@ -3,6 +3,7 @@ type Transaction = {
     amount?: number;
     destination_account?: string;
     description?: string;
+    type: string;
     date?: Date;
     account_id?: number;
     category_id?: number;
