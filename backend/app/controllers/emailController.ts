56f6a3a014bd2a0ffa9a77766d4ac6e569ba3e25
Eduardo Sá <edusrm11@gmail.com>
2023-02-20 22:11:17 -0300

    feat: adicao da funcionalidade envio de email

--- backend/app/controllers/emailController.ts
+++ backend/app/controllers/emailController.ts
@@ -1,15 +1,10 @@
 import 'dotenv/config'
-import nodemailer from 'nodemailer';
+import {Request, Response} from 'express';
 import Email from '../models/Email';
+import { sendEmail } from '../utils/sendEmail';
+import { smtpStatus } from '../utils/smtpStatus';
 
-// Configurações de transporte
-const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
-    auth: {
-        user: process.env.EMAIL_SENDER_USER,
-        pass: process.env.EMAIL_SENDER_PASS
-    }
-});
+
 
 // Configurações de email
 // const mailOptions = {
@@ -20,18 +15,24 @@ const transporter = nodemailer.createTransport({
 // };
 
 // Envia o email
-export const sendEmail = (mailOptions:Email) =>{
-    transporter.sendMail(
-        {
-            from: process.env.EMAIL,
-            to: mailOptions.to,
-            subject: mailOptions.subject,
-            text: mailOptions.text
-        }, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email enviado: ' + info.response);
+export const sendMail = async (req:Request, res:Response) =>{
+    try{
+        let email:Email = req.body;
+        // Configurações de transporte
+        let smtpResponse = await sendEmail(email);
+        let response = smtpResponse.response.split(" ")
+    
+        switch(response[0]){
+            case "250":
+                return res.status(200).json({message: smtpStatus[response[0] as keyof typeof smtpStatus]})
+            case "550":
+                return res.status(400).json({message: smtpStatus[response[0] as keyof typeof smtpStatus]})
+            default:
+                return res.status(500).json({message: smtpStatus[response[0] as keyof typeof smtpStatus]})
         }
-    });
+    }
+    catch(error:any){
+        console.log(error);
+        return res.status(error.statusCode || 500).json({message: error.message || 'Erro no servidor'});
+    }
 }
