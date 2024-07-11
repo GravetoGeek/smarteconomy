64bb8d77752075b2c0147c59b3d148a3a7e0f26a
Eduardo Sá <edusrm11@gmail.com>
2024-07-11 02:33:28 -0300

    clean: Removendo insumos

--- insumos/Codigo_Upmoney/app_upmoney_final/upmoney-master/index.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native'
-import Navigator from './src/Navigator'
-import {name as appName} from './app.json'
-
-AppRegistry.registerComponent(appName, () => Navigator);
