961776188b3599bed5e8cd6c3e536cb2c19ff272
gravetogeek <edusrm11@gmail.com>
2025-01-01 13:50:10 -0300

    :wrench: chore: Configuração do provider UsersRepository

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,24 @@
 import {DatabaseModule} from '@/database/database.module';
+import {PrismaService} from '@/database/prisma/prisma.service';
 import {Module} from '@nestjs/common';
 import {UsersResolver} from './graphql/resolvers/users.resolver';
+import {UsersPrismaRepository} from './repositories/users-prisma.repository';
 
 @Module({
     imports: [DatabaseModule],
-    providers: [UsersResolver]
+    providers: [
+        UsersResolver,
+        {
+            provide: 'PrismaService',
+            useClass: PrismaService
+        },
+        {
+            provide: 'UsersRepository',
+            useFactory: (prisma: PrismaService) => {
+                return new UsersPrismaRepository(prisma)
+            },
+            inject: ['PrismaService']
+        }
+    ]
 })
 export class UsersModule {}
