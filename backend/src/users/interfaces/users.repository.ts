6c74cf03a0effc1634c11c887abe159337752597
gravetogeek <edusrm11@gmail.com>
2025-01-01 13:49:14 -0300

    :sparkles: feat: Implementando casos de uso para criar users

--- backend/src/users/interfaces/users.repository.ts
+++ backend/src/users/interfaces/users.repository.ts
@@ -1,11 +1,30 @@
 import {User} from "../graphql/models/User";
+import {ICreateUser} from "./create-user";
 
+export interface SearchParams {
+    page: number;
+    limit: number;
+    filter?: string;
+    sort?: string;
+    sortDirection?: 'asc'|'desc';
+}
+export interface SearchResult {
+    items: User[];
+    total: number;
+    currentPage: number;
+    limit: number;
+    totalPages: number;
+    lastPage: number;
+
+}
 export interface IUsersRepository {
+    sortableFields: string[];
     findByEmail(email: string): Promise<User>;
-    save(user: any): Promise<User>;
-    update(user: any): Promise<User>;
+    create(user: ICreateUser): Promise<User>;
+    update(user: User): Promise<User>;
     delete(id: string): Promise<User>;
     findById(id: string): Promise<User>;
     findAll(): Promise<User[]>;
     get(id: string): Promise<User>;
+    search(search: SearchParams): Promise<SearchResult>;
 }
