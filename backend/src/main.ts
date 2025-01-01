c2f24d2d3d4801a025baea67d335db308d03f3a6
gravetogeek <edusrm11@gmail.com>
2025-01-01 13:51:32 -0300

    :wrench: chore: Configuração da variável de ambiente BACKEND_PORT

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,16 @@
 import {ValidationPipe} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
+import {DocumentBuilder,SwaggerModule} from '@nestjs/swagger';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
     const app=await NestFactory.create(AppModule);
+
     app.useGlobalPipes(new ValidationPipe({
         whitelist: true,
         forbidNonWhitelisted: true,
         transform: true
     }));
-    await app.listen(process.env.PORT);
+    await app.listen(process.env.BACKEND_PORT);
 }
 bootstrap();
