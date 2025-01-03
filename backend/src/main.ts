33d444fea9d0d3d6f70adb4cd3ef2173affd8675
gravetogeek <edusrm11@gmail.com>
2025-01-02 21:01:55 -0300

    :wastebasket: remove: Remoção de implementação do swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,5 @@
 import {ValidationPipe} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
-import {DocumentBuilder,SwaggerModule} from '@nestjs/swagger';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
