68946671c89c7e6c90f0b413894f07397e2abd63
Eduardo Sá <edusrm11@gmail.com>
2025-08-26 12:09:09 -0300

    :recycle: refactor: Ajusta formatação e espaçamento no módulo accounts e serviços relacionados

--- backend/src/accounts/application/use-cases/find-account-by-id.use-case.ts
+++ backend/src/accounts/application/use-cases/find-account-by-id.use-case.ts
@@ -1,14 +1,14 @@
-import {Inject,Injectable} from '@nestjs/common'
-import {Account} from '../../domain/account.entity'
-import {AccountRepositoryPort} from '../../domain/ports/account-repository.port'
-import {ACCOUNT_REPOSITORY} from '../../domain/tokens'
+import { Inject, Injectable } from '@nestjs/common'
+import { Account } from '../../domain/account.entity'
+import { AccountRepositoryPort } from '../../domain/ports/account-repository.port'
+import { ACCOUNT_REPOSITORY } from '../../domain/tokens'
 
 export interface FindAccountByIdRequest {
     id: string
 }
 
 export interface FindAccountByIdResponse {
-    account: Account|null
+    account: Account | null
 }
 
 @Injectable()
@@ -16,10 +16,10 @@ export class FindAccountByIdUseCase {
     constructor(
         @Inject(ACCOUNT_REPOSITORY)
         private readonly accountRepository: AccountRepositoryPort
-    ) {}
+    ) { }
 
     async execute(request: FindAccountByIdRequest): Promise<FindAccountByIdResponse> {
-        const account=await this.accountRepository.findById(request.id)
-        return {account}
+        const account = await this.accountRepository.findById(request.id)
+        return { account }
     }
 }
