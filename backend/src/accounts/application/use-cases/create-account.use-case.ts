0e061058f7692632e23639362f7cd499379ebbf3
Eduardo Sá <edusrm11@gmail.com>
2025-08-26 12:00:31 -0300

    :sparkles: feat: Implementa módulo accounts (domínio, use-cases, repositório Prisma, GraphQL)

---
+++ backend/src/accounts/application/use-cases/create-account.use-case.ts
@@ -0,0 +1,35 @@
+import {Inject,Injectable} from '@nestjs/common'
+import {Account} from '../../domain/account.entity'
+import {AccountRepositoryPort} from '../../domain/ports/account-repository.port'
+import {ACCOUNT_REPOSITORY} from '../../domain/tokens'
+
+export interface CreateAccountRequest {
+    name: string
+    type: string
+    balance?: number
+    userId: string
+}
+
+export interface CreateAccountResponse {
+    account: Account
+}
+
+@Injectable()
+export class CreateAccountUseCase {
+    constructor(
+        @Inject(ACCOUNT_REPOSITORY)
+        private readonly accountRepository: AccountRepositoryPort
+    ) {}
+
+    async execute(request: CreateAccountRequest): Promise<CreateAccountResponse> {
+        const account=new Account({
+            name: request.name,
+            type: request.type as any,
+            balance: request.balance||0,
+            userId: request.userId
+        })
+
+        const saved=await this.accountRepository.save(account)
+        return {account: saved}
+    }
+}
